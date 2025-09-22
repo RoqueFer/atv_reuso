f617e63c21eb77691c830414e13a3eceaa1ad303
unknown <fdestroque@gmail.com>
2025-09-22 17:40:09 -0300

    path corrigido

--- clientes/gerenciador_clientes.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # Adicione 'include'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Inclui todas as URLs do arquivo 'clientes/urls.py'
-    # Qualquer acesso a 'api/' será redirecionado para o nosso app de clientes
-    path('api/', include('clientes.urls')), 
-]
